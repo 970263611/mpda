0c1bb23c5178c48fce9f4756627edef7840f0184
dahua <970263611@qq.com>
2025-09-22 11:13:14 +0800

    添加监控

---
+++ core/src/main/java/com/dahuaboke/mpda/core/monitor/MonitorController.java
@@ -0,0 +1,28 @@
+package com.dahuaboke.mpda.core.monitor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+/**
+ * 2025/9/22 9:28
+ * auth: dahua
+ * desc:
+ */
+@RestController
+public class MonitorController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MonitorController.class);
+
+    @Autowired
+    private MonitorManager monitorManager;
+
+    @RequestMapping(value = "/monitor", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux monitor() {
+        return monitorManager.monitor();
+    }
+}
--- core/src/main/java/com/dahuaboke/mpda/core/monitor/MonitorManager.java
+++ core/src/main/java/com/dahuaboke/mpda/core/monitor/MonitorManager.java
@@ -1,12 +1,10 @@
 package com.dahuaboke.mpda.core.monitor;
 
-import com.dahuaboke.mpda.core.context.CacheManager;
 import com.dahuaboke.mpda.core.event.Event;
 import com.dahuaboke.mpda.core.event.Listener;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
 
 /**
  * auth: dahua
@@ -15,17 +13,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class MonitorManager implements Listener {
 
-    @Autowired
-    private CacheManager cacheManager;
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final Sinks.Many<Event> sink = Sinks.many().multicast().onBackpressureBuffer();
 
     @Override
     public void onEvent(Event event) {
-        try {
-            System.out.println(objectMapper.writeValueAsString(event)); // TODO 完善监控
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        sink.tryEmitNext(event); // TODO 完善监控
+    }
+
+    public Flux monitor() {
+        return sink.asFlux()
+                .doOnCancel(() -> System.out.println("Monitor disconnected from stream"))
+                .doOnError(e -> System.err.println("Error occurred during streaming: " + e));
     }
 }
